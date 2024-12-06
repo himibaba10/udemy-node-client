e20c93411489b14e87b230c8a9a801f6fe4829f9
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-12-06 14:48:34 +0600

    Cleaned some code and logs

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -182,14 +182,6 @@ class Feed extends Component {
         return res.json();
       })
       .then((resData) => {
-        console.log({ resData });
-        const post = {
-          _id: resData.post._id,
-          title: resData.post.title,
-          content: resData.post.content,
-          creator: resData.post.creator,
-          createdAt: resData.post.createdAt,
-        };
         this.setState((prevState) => {
           return {
             isEditing: false,
